53463104fb83266e445332c58d1943fefac6f83c
Matt <meinzer.matthew@gmail.com>
2018-04-25 22:18:25 -0500

    Build out apartment component with basic styles

--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -3,9 +3,31 @@ import React from "react";
 const ApartmentsList = props => {
   return (
     <div className="apartments">
-      <ul>{props.apartments.map(item => <li key={item.id}>{item.url}</li>)}</ul>
+      <ul style={ulStyle}>
+        {props.apartments.map(item => (
+          <li key={item.id} style={liStyle}>
+            <a href={item.url} style={nameStyle} target="_blank">
+              {item.name}
+            </a>
+            <div>{item.neighborhood}</div>
+            <div>{item.address}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+};
+
+const ulStyle = {
+  listStyleType: "none"
+};
+
+const liStyle = {
+  display: "grid"
+};
+
+const nameStyle = {
+  fontSize: "1.5em"
 };
 
 export default ApartmentsList;
